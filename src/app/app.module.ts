72328aa47d1719995d5b3aacf666ed3574e01de4
judith-bring <judith.mwalo@gmail.com>
2022-03-14 02:28:08 +0300

    update app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/shared-components/search-bar/search-bar.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { ArtistDetailsComponent } from './components/artist-details/artist-details.component';
+import { ArtistsComponent } from './components/artists/artists.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    AlbumsComponent,
+    ArtistDetailsComponent,
+    ArtistsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
