5a6725f3e54ce7259149a8e34a0eed2ac8bd8119
judith-bring <judith.mwalo@gmail.com>
2022-03-14 04:04:54 +0300

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/',
+  searchUrl: 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q="'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/',
+  searchUrl: 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q="'
 };
 
 /*
